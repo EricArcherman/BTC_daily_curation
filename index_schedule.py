a84ec92e4e3f517b794113bc95c4befb1d058f8c
dashan0313 <hjs349960325@163.com>
2024-11-19 17:58:04 +0800

    hotfix

--- index_schedule.py
+++ index_schedule.py
@@ -26,10 +26,10 @@ def send_today_index():
 timez = pytz.timezone('Asia/Shanghai')
 
 scheduler = BlockingScheduler(timezone=timez)
-# scheduler.add_job(send_yesterday_index, 'cron', hour=send_yesterday_time[0], minute=send_yesterday_time[1])
-# scheduler.add_job(send_today_index, 'cron', hour=send_today_time[0], minute=send_today_time[1])
-scheduler.add_job(send_yesterday_index, 'cron', second=30)
-scheduler.add_job(send_today_index, 'cron', second=1)
+scheduler.add_job(send_yesterday_index, 'cron', hour=send_yesterday_time[0], minute=send_yesterday_time[1])
+scheduler.add_job(send_today_index, 'cron', hour=send_today_time[0], minute=send_today_time[1])
+# scheduler.add_job(send_yesterday_index, 'cron', second=30)
+# scheduler.add_job(send_today_index, 'cron', second=1)
 
 try:
     print("Start!")
