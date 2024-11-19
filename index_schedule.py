6a40ac2fda36ecda5af102debfe2b472f6c73dbf
dashan0313 <hjs349960325@163.com>
2024-11-19 17:56:37 +0800

    hotfix

--- index_schedule.py
+++ index_schedule.py
@@ -1,30 +1,27 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import pytz
 
-from data.update import main as update_data
-from extract import main as extract_data
 from sender.lark_text import yesterday_data
 from sender.lark_text import today_data
 
-send_yesterday_time = (8,30)
+send_yesterday_time = (8, 30)
 send_today_time = (10, 5)
+
 
 def send_yesterday_index():
     try:
-        update_data()
-        extract_data()
         yesterday_data()
         print(f"Will send tomorrow's index data at {send_yesterday_time[0]}:{send_yesterday_time[1]}.")
     except Exception as e:
         print(f"send_yesterday_index meet error: {e}")
 
+
 def send_today_index():
     try:
-        update_data()
-        extract_data()
         today_data()
     except Exception as e:
         print(f"send_today_index meet error: {e}")
+
 
 timez = pytz.timezone('Asia/Shanghai')
 
@@ -37,6 +34,7 @@ scheduler.add_job(send_today_index, 'cron', second=1)
 try:
     print("Start!")
     scheduler.start()
-    print(f"Index data sender process initiated. The data will be sent today at {send_today_time[0]}:{send_today_time[1]}.")
+    print(
+        f"Index data sender process initiated. The data will be sent today at {send_today_time[0]}:{send_today_time[1]}.")
 except (KeyboardInterrupt, SystemExit):
     pass
--- sender/lark_text.py
+++ sender/lark_text.py
@@ -1,6 +1,15 @@
 import json
+
+import numpy as np
 import requests
 import pandas as pd
+from datetime import datetime, timedelta
+import pytz
+
+
+def is_hour(ts):
+    return ts % (1000 * 3600) == 0
+
 
 def fetch_data():
     file_path = 'extracted_prices.csv'
@@ -12,6 +21,7 @@ def fetch_data():
     today_formatted = '\t'.join(map(str, today.values))
 
     return yesterday_formatted, today_formatted
+
 
 def lark_data_loader(message):
     try:
@@ -28,15 +38,77 @@ def lark_data_loader(message):
     except requests.exceptions.ProxyError as e:
         print(f"requests.exceptions.ProxyError {e}, please check your proxy")
 
+
 def yesterday_data():
-    target_message = fetch_data()
-    print(f"Here's yesterday's data:\n{target_message[0]}")
-    lark_data_loader(f"Here's yesterday's data:\n{target_message[0]}")
+    # 获取当前时间
+    now = datetime.now()
+    # 设置时区为UTC+8
+    utc8 = pytz.timezone('Asia/Shanghai')
+
+    # 将当前时间转换为UTC+8时区时间
+    now_utc8 = utc8.localize(now)
+
+    # 获取当天的0点时间戳
+    midnight_utc8 = utc8.localize(datetime(now_utc8.year, now_utc8.month, now_utc8.day))
+
+    # 获取前一天的0点时间戳
+    yesterday_midnight_utc8 = midnight_utc8 - timedelta(days=1)
+    yesterday_date = yesterday_midnight_utc8.date()
+
+    # 将时间转换为时间戳
+    midnight_timestamp = int(midnight_utc8.timestamp()) * 1000
+    yesterday_midnight_timestamp = int(yesterday_midnight_utc8.timestamp()) * 1000
+
+    start_time = yesterday_midnight_timestamp - 1000
+    end_time = midnight_timestamp + 1000
+    url = f"https://mizar-gateway.signalplus.net/mizar/index_price?exchange=deribit&currency=BTC&startTime={start_time}&endTime={end_time}"
+    res = requests.get(url).json()
+    res_dict = pd.DataFrame(res["value"])
+    is_hour_list = res_dict["timestamp"].apply(lambda x: is_hour(x))
+    res_dict = res_dict.loc[is_hour_list, :].reset_index(drop=True)
+    data_message = '\t'.join(
+        map(str, list(res_dict["indexPrice"].values) + [np.nan] * (24 - len(res_dict["indexPrice"].values))))
+    full_message = f"Here's yesterday's's data:\n{yesterday_date} {data_message}"
+    print(full_message)
+    lark_data_loader(f"{full_message}")
+
 
 def today_data():
-    target_message = fetch_data()
-    print(f"Here's today's data:\n{target_message[1]}")
-    lark_data_loader(f"Here's today's data:\n{target_message[1]}")
+    # 获取当前时间
+    now = datetime.now()
+    current_date = datetime.now().date()
+    # 设置时区为UTC+8
+    utc8 = pytz.timezone('Asia/Shanghai')
+
+    # 将当前时间转换为UTC+8时区时间
+    now_utc8 = utc8.localize(now)
+
+    # 获取当天的0点时间戳
+    midnight_utc8 = utc8.localize(datetime(now_utc8.year, now_utc8.month, now_utc8.day))
+
+    # 获取当天的10点时间戳
+    ten_am_utc8 = midnight_utc8.replace(hour=10)
+
+    # 获取前一天的0点时间戳
+    yesterday_midnight_utc8 = midnight_utc8 - timedelta(days=1)
+
+    # 将时间转换为时间戳
+    midnight_timestamp = int(midnight_utc8.timestamp()) * 1000
+    ten_am_timestamp = int(ten_am_utc8.timestamp()) * 1000
+
+    start_time = midnight_timestamp - 1000
+    end_time = ten_am_timestamp + 1000
+    url = f"https://mizar-gateway.signalplus.net/mizar/index_price?exchange=deribit&currency=BTC&startTime={start_time}&endTime={end_time}"
+    res = requests.get(url).json()
+    res_dict = pd.DataFrame(res["value"])
+    is_hour_list = res_dict["timestamp"].apply(lambda x: is_hour(x))
+    res_dict = res_dict.loc[is_hour_list, :].reset_index(drop=True)
+    data_message = '\t'.join(
+        map(str, list(res_dict["indexPrice"].values) + [np.nan] * (24 - len(res_dict["indexPrice"].values))))
+    full_message = f"Here's today's data:\n{current_date} {data_message}"
+    print(full_message)
+    lark_data_loader(full_message)
+
 
 if __name__ == "__main__":
     today_data()
