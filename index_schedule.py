7f6f3defd57cca3332e1ead6c4cf069b7caaad96
dashan0313 <hjs349960325@163.com>
2024-11-19 16:55:26 +0800

    hotfix

--- index_schedule.py
+++ index_schedule.py
@@ -10,22 +10,28 @@ send_yesterday_time = (8,30)
 send_today_time = (10, 5)
 
 def send_yesterday_index():
-    update_data()
-    extract_data()
-    yesterday_data()
-    print(f"Will send tomorrow's index data at {send_yesterday_time[0]}:{send_yesterday_time[1]}.")
+    try:
+        update_data()
+        extract_data()
+        yesterday_data()
+        print(f"Will send tomorrow's index data at {send_yesterday_time[0]}:{send_yesterday_time[1]}.")
+    except Exception as e:
+        print(f"send_yesterday_index meet error: {e}")
 
 def send_today_index():
-    update_data()
-    extract_data()
-    today_data()
+    try:
+        update_data()
+        extract_data()
+        today_data()
+    except Exception as e:
+        print(f"send_today_index meet error: {e}")
 
 timez = pytz.timezone('Asia/Shanghai')
 
 scheduler = BlockingScheduler(timezone=timez)
 # scheduler.add_job(send_yesterday_index, 'cron', hour=send_yesterday_time[0], minute=send_yesterday_time[1])
 # scheduler.add_job(send_today_index, 'cron', hour=send_today_time[0], minute=send_today_time[1])
-scheduler.add_job(send_yesterday_index, 'cron', second=1)
+scheduler.add_job(send_yesterday_index, 'cron', second=30)
 scheduler.add_job(send_today_index, 'cron', second=1)
 
 try:
